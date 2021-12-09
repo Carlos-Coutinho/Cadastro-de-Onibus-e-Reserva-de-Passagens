76f0d19df2bc59ad5e5326564d19ed1ef4328740
Lucas Eduardo Lima Da Silva <lucaspity721@gmail.com>
2021-12-09 00:00:55 -0300

    Substiuição De Struct Por Struct Vetor Com For Para Cadastros

--- Cadastro_Ônibus.c
+++ Cadastro_Ônibus.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 #include <locale.h>
 
-struct cadastro_onibus{
-int poltronas;
-char tipo_viagem[10];
-int codigo_onibus;
-char destino[20];
-}x;
+# define indiceonibus 2 
+struct cadastro_onibus
+{
+    int poltronas;
+    char tipo_viagem[10];
+    int codigo_onibus;
+    char destino[20];
+} x[indiceonibus];
 
-int main(){
+int main()
+{
 
-setlocale(LC_ALL,"Portuguese");
+    setlocale(LC_ALL, "Portuguese");
 
-printf("\nDigite o código do ônibus (001): ");
-scanf("%d",&x.codigo_onibus);
-printf("\nDigite o tipo de viagem que o ônibus realiza (Estadual ou Municipal): ");
-scanf("%s",x.tipo_viagem);
-printf("\nDigite o número de poltronas que o ônibus possui: ");
-scanf("%d",&x.poltronas);
-printf("\nDigite a cidade de destino do ônibus (Caxias-MA): ");
-scanf("%s",&x.destino);
+    int i;
 
-printf("\n O código do ônibus é %d, ele faz viagens do tipo %s, ele possui %d poltonas e o seu destino é %s\n",x.codigo_onibus,x.tipo_viagem,x.poltronas,x.destino);
+    for (i = 0; i < indiceonibus; i++)
+    {
+        printf("_________________________________________");
+        printf("\nDigite o código do ônibus (00%d): ",i);
+        scanf("%d", &x[i].codigo_onibus);
+        printf("\nDigite o tipo de viagem que o ônibus realiza (Estadual ou Municipal): ");
+        scanf("%s", x[i].tipo_viagem);
+        printf("\nDigite o número de poltronas que o ônibus possui: ");
+        scanf("%d", &x[i].poltronas);
+        printf("\nDigite a cidade de destino do ônibus (Caxias-MA): ");
+        scanf("%s", &x[i].destino);
 
-return 0;
+        printf("\n O código do ônibus é %d, ele faz viagens do tipo %s, ele possui %d poltonas e o seu destino é %s\n", x[i].codigo_onibus, x[i].tipo_viagem, x[i].poltronas, x[i].destino);
+    }
+
+    return 0;
 }
-
-    
